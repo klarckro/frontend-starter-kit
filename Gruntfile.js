1cf786eb395b7f1c7aa3ab9bbb7d2ed60d691ffc
Robin Klärck <r.klaerck@insign.ch>
2016-07-13 17:12:56 +0200

    Updated grunt-sass module for faster compilation

--- Gruntfile.js
+++ Gruntfile.js
@@ -249,7 +249,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-include-replace');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-html');
     grunt.loadNpmTasks('grunt-csscomb');
     grunt.loadNpmTasks("grunt-jscs");
